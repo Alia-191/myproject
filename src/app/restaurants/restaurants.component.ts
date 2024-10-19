468a6ecd2541c758efcc717d3dfa1619181faa72
aliaaabdelhameed <aliaaabdelhameed7@gmail.com>
2024-10-19 18:58:09 +0300

    Add new files

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-restaurants',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurants.component.html',
+  styleUrl: './restaurants.component.css'
+})
+export class RestaurantsComponent {
+
+
+
+}
+
